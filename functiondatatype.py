957cdc92db757bd7c9494087875e22c0f0fc3466
sonnylmao <69658757+sonnylmao@users.noreply.github.com>
2020-09-30 08:42:22 +0800

    Update functiondatatype.py

--- functiondatatype.py
+++ functiondatatype.py
@@ -9,6 +9,6 @@ def randomtype():
   nice.append(random.choice(string.ascii_letters))#CHAR
   nice.append("".join(random.choice(string.ascii_letters) for x in range(random.randint(10,30)))) #STRING
   nice.append(random.choice(["TRUE","FALSE"])) #BOOLEAN
-  nice.append(datetime.date(random.randint(1900,2100),random.randint(1,12),random.randint(1,28)))
+  nice.append(datetime.date(random.randint(1900,2100),random.randint(1,12),random.randint(1,28))) #DATE
   print(*nice,sep="\n")
 print(randomtype())
