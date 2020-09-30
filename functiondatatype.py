3d21053d3f2b19a31a791953abc0a299d194a0a1
sonnylmao <69658757+sonnylmao@users.noreply.github.com>
2020-09-30 08:40:38 +0800

    Create functiondatatype.py

---
+++ functiondatatype.py
@@ -0,0 +1,14 @@
+import random
+import string
+import datetime
+nice = []
+
+def randomtype():
+  nice.append(random.randint(1,6969)) #INTEGER
+  nice.append(random.uniform(1,6969)) #REAL
+  nice.append(random.choice(string.ascii_letters))#CHAR
+  nice.append("".join(random.choice(string.ascii_letters) for x in range(random.randint(10,30)))) #STRING
+  nice.append(random.choice(["TRUE","FALSE"])) #BOOLEAN
+  nice.append(datetime.date(random.randint(1900,2100),random.randint(1,12),random.randint(1,28)))
+  print(*nice,sep="\n")
+print(randomtype())
