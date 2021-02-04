aadcbdac876ea35a34003a5c2189de9ab6b7f2b2
sonnylmao <69658757+sonnylmao@users.noreply.github.com>
2021-02-04 18:51:11 +0800

    Update capstoneproject.py
    version2

--- capstoneproject.py
+++ capstoneproject.py
@@ -1,8 +1,9 @@
 import string
 import random
+import datetime
 import time
 import math
-from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style #pip install colorama
 
 lives = 3
 score = 0
@@ -15,16 +16,10 @@ def lines():
     print("===============================")
 
 def correct():
-    print(Fore.LIGHTGREEN_EX + "Correct!")
-    score += 1
-    print("score: ",score)
-    time.sleep(1)
+    print(Fore.LIGHTGREEN_EX + "correct!")
 
 def wrong():
     print(Fore.RED + "wrong!")
-    lives -= 1
-    print("remaining lives: ",lives)
-    time.sleep(1)
     
 lines()
 print(" welcome to the revision game!")
@@ -35,22 +30,27 @@ time.sleep(1)
 while lives != 0:
     empty(1)
     print(Style.RESET_ALL)
-    print("round:", c)
+    print("[ round:", c,"]")
     empty(1)
     ranstring = "".join(random.choice(string.ascii_letters) for x in range(random.randint(10,20)))
-    #a = random.randint(1,3)
-    a = 2
+    a = random.randint(1,3)
     
-    #simple 
+    #easy round 
     if a == 1:
         b = random.randint(0,len(ranstring))
         print(ranstring+"[%s]" % str(b))
         userinp = input("what would this be?: ")
         if userinp == ranstring[b]:
             correct()
+            score += 1
+            print("score:",score)
+            time.sleep(1)
         else:
             wrong()
-
+            lives -= 1
+            print("remaining lives:",lives)
+            time.sleep(1)
+    
     #a bit more complicated, yikes!
     elif a == 2:
         b = random.randint(0,len(ranstring)-math.floor(len(ranstring)/2))
@@ -59,8 +59,14 @@ while lives != 0:
         userinp = input("what would this be?: ")
         if userinp == ranstring[b:c]:
             correct()
+            score += 1
+            print("score:",score)
+            time.sleep(1)
         else:
             wrong()
+            lives -= 1
+            print("remaining lives: ",lives)
+            time.sleep(1)
 
     #super hard!!! omgg!!
     else:
@@ -71,15 +77,27 @@ while lives != 0:
             userinp = input("what would this be?: ")
             if userinp == ranstring[:b]:
                 correct()
+                score += 1
+                print("score:",score)
+                time.sleep(1)
             else:
                 wrong()
+                lives -= 1
+                print("remaining lives:",lives)
+                time.sleep(1)
         else:
             print(ranstring+"[%s:]" % str(b))    
             userinp = input("what would this be?: ")
             if userinp == ranstring[b:]:
                 correct()
+                score += 1
+                print("score:",score)
+                time.sleep(1)
             else:
                 wrong()
+                lives -= 1
+                print("remaining lives:",lives)
+                time.sleep(1)
     c += 1
 
 print(Style.RESET_ALL)
@@ -87,5 +105,6 @@ print("Thank you for playing!")
 print("Saving highscore...")
 
 f = open("highscores.txt","a")
-f.write(username,score)
+f.write("\n %s \n" % datetime.datetime.now())
+f.write("%s : %s " % (username, score))
 f.close()
